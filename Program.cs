f536a9c665b74181557d4c25fb6464cb66a90eb0
EliasRoro <eliassaychelem@gmail.com>
2020-02-25 16:37:18 -0500

    Triangle Class Created

--- Program.cs
+++ Program.cs
@@ -10,7 +10,52 @@ namespace ERAssignemnt2SQA
     {
         static void Main(string[] args)
         {
+            int menu = 0;
+            List<int> sides = new List<int>();
+
+            while (menu != 2)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Enter triangle dimensions");
+                Console.WriteLine("2. Exit\n");
+
+                while (!int.TryParse(Console.ReadLine(), out menu) || menu <= 0 || menu > 2)
+                {
+                    Console.WriteLine("\nEnter a Right Number!");
+                }
+                Console.WriteLine();
+
+                int result = 0;
+                switch (menu)
+                {
+                    case 1:
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine($"Enter number {i}:");
+
+                            while (true)
+                            {
+                                if (int.TryParse(Console.ReadLine(), out result))
+                                {
+                                    sides.Add(result);
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("It must be a positive integer!");
+                                    Console.WriteLine("Re-enter integer");
+                                }
+                            }
+                        }
+                        Console.WriteLine(TriangleClass.Analyze(ref sides));
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+            }
 
         }
     }
+    
 }
---
+++ TriangleClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERAssignemnt2SQA
+{
+    class TriangleClass
+    {
+
+        public static string Analyze(ref List<int> sides)
+        {
+            int maxSide = sides.Max();
+            sides.Remove(maxSide);
+
+            int minSide = sides.Min();
+            sides.Remove(minSide);
+
+            int mediumSide = sides[0];
+            sides.Remove(mediumSide);
+
+            string output = "The sides do not form a triangle!";
+
+            if ((minSide + mediumSide) - maxSide > 0)
+            {
+                if (minSide == maxSide)
+                {
+                    output = "This is an equilateral triangle!";
+                }
+                else if (minSide == mediumSide || minSide == maxSide || mediumSide == maxSide)
+                {
+                    output = "This is an isosceles triangle!";
+                }
+                else
+                {
+                    output = "This is an scalene triangle!";
+                }
+            }
+
+            return output;
+        }
+    }
+}
